73b1f9fd0a572b36c37f59901fe961da01533452
Shuai-Hsun <victory2152@livemail.tw>
2017-12-11 14:51:27 -0800

    addfeature: wifi status, modi:hostname to wifi

---
+++ segments/wifi.sh
@@ -0,0 +1,50 @@
+# Prints the wifi status.
+
+TMUX_POWERLINE_SEG_WIFI_FORMAT_DEFAULT="both"
+
+generate_segmentrc() {
+	read -d '' rccontents  << EORC
+# Use status or wifiname or both format for the wifi. Can be {"status, wifiname, both"}.
+export TMUX_POWERLINE_SEG_WIFI_FORMAT="${TMUX_POWERLINE_SEG_WIFI_FORMAT_DEFAULT}"
+EORC
+	echo "$rccontents"
+}
+
+__process_settings() {
+	if [ -z "$TMUX_POWERLINE_SEG_WIFI_FORMAT" ]; then
+		export TMUX_POWERLINE_SEG_WIFI_FORMAT="${TMUX_POWERLINE_SEG_WIFI_FORMAT_DEFAULT}"
+	fi
+}
+run_segment() {
+	__process_settings
+	local opts=""
+	if [ "$TMUX_POWERLINE_SEG_WIFI_FORMAT" == "status" ]; then
+        wifi=$(__status)
+    elif [ "$TMUX_POWERLINE_SEG_WIFI_FORMAT" == "wifiname" ]; then
+        wifi=$(__wifiname)
+    else
+        wifi="$(__status)$(__wifiname)"
+	fi
+
+	echo $wifi
+	return 0
+}
+
+__status() {
+    status=$(networksetup -getairportpower en0 | sed 's/.*: \([a-zA-Z\.]*\)$/\1/')
+    if [ $status == "On" ]; then
+        echo "◉ "
+    else
+        echo "◎ "
+    fi
+}
+
+__wifiname() {
+    status=$(networksetup -getairportpower en0 | sed 's/.*: \([a-zA-Z\.]*\)$/\1/')
+    wifiname=$(networksetup -getairportnetwork en0 | sed 's/.*: \([a-zA-Z\.]*\)$/\1/')
+    if [ $status == "On" ]; then
+        echo $wifiname
+    else
+        echo "OFF"
+    fi
+}
